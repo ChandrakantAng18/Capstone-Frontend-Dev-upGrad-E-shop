612af023d313a944376dd28a7f572ebf64efbb1a
Chandrakant Dighade <mailatchandrakant@gmail.com>
2022-03-25 11:02:19 +0530

    Checkpoint 1(Starter Code and Configuration Guidelines)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is upGrad E-shop</h1>
+      <h4>Checkpoint 1</h4>
+      <h4>Starter Code and Configuration Guidelines</h4>
+      <h5>
+        Successfully created react app (upGrad E-shop)and created folder
+        structure as per upGrad guidelines.
+      </h5>
     </div>
   );
 }
